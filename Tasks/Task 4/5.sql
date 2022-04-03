cb2099868b10763e4b29f45079d846460236890d
xNikolaussx <max.eisenhardt1943@yandex.ru>
2022-04-03 16:06:14 +0300

    Лабораторная работа № 4 #14
    Завершено выполнение лабораторной работы № 4

---
+++ Tasks/Task 4/5.sql
@@ -0,0 +1,11 @@
+﻿--Показать товары, цена которых больше средней цены в любой подкатегории 
+--(Таблица Production.Product). 
+--Показать поля [Name], ListPrice и ProductSubcategoryID.
+SELECT [Name], ListPrice, ProductSubcategoryID 
+FROM Production.Product
+WHERE ListPrice > ALL(
+	SELECT AVG(ListPrice) AS ListAvgPrice
+	FROM Production.Product
+	WHERE ProductSubcategoryID IS NOT NULL
+	GROUP BY ProductSubcategoryID
+	);
